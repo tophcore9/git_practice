bf0d51ea58ba546bcaf7f2a4fee0256fc814c971
tophcore9 <alexeypazyura@gmail.com>
2024-07-10 18:07:48 +0300

    Added task_04.py and info.txt for it

---
+++ task_04.py
@@ -0,0 +1,15 @@
+def checkio(words: str) -> bool:
+    return 0
+
+
+print("Example:")
+print(checkio("Hello World hello"))
+
+# These "asserts" are used for self-checking
+assert checkio("Hello World hello") == True
+assert checkio("He is 123 man") == False
+assert checkio("1 2 3 4") == False
+assert checkio("bla bla bla bla") == True
+assert checkio("Hi") == False
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
